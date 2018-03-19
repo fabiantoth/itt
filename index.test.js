2359ec7007ef760e00cee6b28b60bd7a7233f17e
Nathan <nathan@users.noreply.github.com>
2018-03-19 12:25:39 -0400

    Test range

--- index.test.js
+++ index.test.js
@@ -30,6 +30,32 @@ test('empty', () => {
   expect(Array.from(itt.empty())).toEqual([])
 })
 
+describe('range', () => {
+  test('returns wrapped iterators', () => {
+    expect(itt.range(5).toArray).toBeDefined()
+  })
+  test('accepts one argument', () => {
+    expect(Array.from(itt.range(5))).toEqual([0, 1, 2, 3, 4])
+    expect(Array.from(itt.range(10))).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    expect(Array.from(itt.range(0))).toEqual([])
+  })
+  test('accepts two arguments', () => {
+    expect(Array.from(itt.range(-2, 2))).toEqual([-2, -1, 0, 1])
+    expect(Array.from(itt.range(1, 5))).toEqual([1, 2, 3, 4])
+    expect(Array.from(itt.range(5, 5))).toEqual([])
+    expect(Array.from(itt.range(5, 1))).toEqual([])
+  })
+  test('accepts three arguments', () => {
+    expect(Array.from(itt.range(4, 13, 3))).toEqual([4, 7, 10])
+    expect(Array.from(itt.range(5, -5, -1))).toEqual([5, 4, 3, 2, 1, 0, -1, -2, -3, -4])
+    expect(Array.from(itt.range(5, -5, -2))).toEqual([5, 3, 1, -1, -3])
+    expect(Array.from(itt.range(-5, 10, -1))).toEqual([])
+    expect(Array.from(itt.range(5, 5, 3))).toEqual([])
+    expect(Array.from(itt.range(5, 5, -3))).toEqual([])
+    expect(Array.from(itt.range(5, 1, 3))).toEqual([])
+  })
+})
+
 test('mean', () => {
   expect(itt.mean([1, 2, 3, 4])).toBe(2.5)
   expect(itt.mean([])).toBe(NaN)
