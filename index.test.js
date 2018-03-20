67bfd424a3e2e3aa00880ef38fddff89bddd7f2a
Nathan <nathan@users.noreply.github.com>
2018-03-20 17:02:56 -0400

    Fix test description

--- index.test.js
+++ index.test.js
@@ -947,7 +947,7 @@ describe('indexOf', () => {
     expect(itt.indexOf(-0, [1, 1, 0])).toBe(2)
     expect(itt.indexOf(NaN, [NaN, NaN, NaN])).toBe(-1)
   })
-  test(`short-circuits when an element satisfies fn`, () => {
+  test(`short-circuits when an element is identical to x`, () => {
     let it = false
     const i = itt.indexOf(1, function*() {yield 1; it = true; yield 2}())
     expect(it).toBe(false)
