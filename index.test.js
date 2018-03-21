281460655177b280d9e29bdd8281fe81b54aca4a
Nathan <nathan@users.noreply.github.com>
2018-03-21 10:49:15 -0400

    toBe{Truthy,Falsy} => toBe({true,false})

--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
 const itt = require('itt')
 
 test('is', () => {
-  expect(itt.is(1)).toBeFalsy()
-  expect(itt.is({})).toBeFalsy()
-  expect(itt.is(Object)).toBeFalsy()
-  expect(itt.is('test')).toBeTruthy()
-  expect(itt.is('test'[Symbol.iterator]())).toBeTruthy()
-  expect(itt.is([1, 2, 3])).toBeTruthy()
-  expect(itt.is([1, 2, 3][Symbol.iterator]())).toBeTruthy()
-  expect(itt.is(itt.range(5))).toBeTruthy()
+  expect(itt.is(1)).toBe(false)
+  expect(itt.is({})).toBe(false)
+  expect(itt.is(Object)).toBe(false)
+  expect(itt.is('test')).toBe(true)
+  expect(itt.is('test'[Symbol.iterator]())).toBe(true)
+  expect(itt.is([1, 2, 3])).toBe(true)
+  expect(itt.is([1, 2, 3][Symbol.iterator]())).toBe(true)
+  expect(itt.is(itt.range(5))).toBe(true)
 })
 
 describe('generator', () => {
