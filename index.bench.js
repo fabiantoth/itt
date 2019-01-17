464da4093a42da5ae4a30a2bd5630d1b8734b476
Nathan <nathan@users.noreply.github.com>
2019-01-17 16:20:31 -0500

    Some more benchmarking

--- index.bench.js
+++ index.bench.js
@@ -1,20 +1,24 @@
 const itt = require('.')
 const benchmark = require('benchmark')
 
+const s = '1,'.repeat(1000000) + '1'
+
 const suite = new benchmark.Suite()
 .add('native', () => {
+  // s.split(',').reduce((a, b) => a + b, 0)
   // Array(10000).fill(0).map(x => x + 1)
-  Array(10000).fill(0)
+  Array(100000).fill(0)
   .map(x => x + 1)
   .filter(x => x % 7 !== 1)
   .reduce((a, b) => a + b, 0)
 })
 .add('itt', () => {
-  // itt(Array(10000).fill(0)).map(x => x + 1).toArray()
-  itt(Array(10000).fill(0))
+  // itt.split(s, ',').sum()
+  // itt.replicate(10000, 0).map(x => x + 1).toArray()
+  itt.replicate(100000, 0)
   .map(x => x + 1)
   .filter(x => x % 7 !== 1)
-  .reduce((a, b) => a + b, 0)
+  .sum()
 })
 .on('error', event => console.log(event.target.error.stack))
 .on('cycle', event => console.log(''+event.target))
